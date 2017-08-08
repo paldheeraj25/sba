83e9ab6e54e4dc8a9b65ab8b5ebd0de55b57e7e4
Dheeraj <dheeraj.pal@rubiconred.com>
2017-08-08 05:37:44 +0530

    npm module

--- app/dashboard/module.js
+++ app/dashboard/module.js
@@ -1,33 +1,61 @@
 'use strict';
 
 angular.module('app.dashboard', [
-    'ui.router',
-    'ngResource'
+  'ui.router',
+  'ngResource'
 ])
 
-.config(function ($stateProvider) {
+  .config(function ($stateProvider) {
     $stateProvider
-        .state('app.dashboard', {
-            url: '/dashboard',
-            views: {
-                "content@app": {
-                    controller: 'DashboardCtrl',
-                    templateUrl: 'app/dashboard/dashboard.html'
+      .state('app.dashboard', {
+        url: '/dashboard',
+        views: {
+          "content@app": {
+            controller: 'DashboardCtrl',
+            templateUrl: 'app/dashboard/dashboard.html'
+          }
+        },
+        data: {
+          title: 'Dashboard'
+        }
+      })
+      .state('app.dashboard-social', {
+        url: '/dashboard-social',
+        views: {
+          "content@app": {
+            templateUrl: 'app/dashboard/social-wall.html'
+          }
+        },
+        data: {
+          title: 'Dashboard Social'
+        }
+      }).state('app.dashboard-upload-excel', {
+        url: '/dashboard-upload-excel',
+        views: {
+          "content@app": {
+            templateUrl: 'app/dashboard/upload-excel.tpl.html',
+            controller: function ($scope) {
+              $scope.dropzoneConfig = {
+                'options': { // passed into the Dropzone constructor
+                  'url': '/api/plug'
+                },
+                'eventHandlers': {
+                  'sending': function (file, xhr, formData) {
+                  },
+                  'success': function (file, response) {
+                  }
                 }
+              };
             },
-            data:{
-                title: 'Dashboard'
+            resolve: {
+              scripts: function (lazyScript) {
+                return lazyScript.register('build/vendor.ui.js')
+              }
             }
-        })
-        .state('app.dashboard-social', {
-            url: '/dashboard-social',
-            views: {
-                "content@app": {
-                    templateUrl: 'app/dashboard/social-wall.html'
-                }
-            },
-            data:{
-                title: 'Dashboard Social'
-            }
-        });
-});
+          }
+        },
+        data: {
+          title: 'Dashboard Upload Excel'
+        }
+      });
+  });
