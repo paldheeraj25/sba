60b217736e8b37a35df0e7f3424b74cd55fc0629
Dheeraj <dheeraj.pal@rubiconred.com>
2017-08-11 04:52:24 +0530

    added more files

--- app/smart-admin/module.js
+++ app/smart-admin/module.js
@@ -6,59 +6,70 @@ angular.module('app.smartAdmin', ['ui.router']);
 
 angular.module('app.smartAdmin').config(function ($stateProvider) {
 
-    $stateProvider
-        .state('app.smartAdmin', {
-            abstract: true,
-            data: {
-                title: 'SmartAdmin Intel'
-            }
-        })
+  $stateProvider
+    .state('app.smartAdmin', {
+      abstract: true,
+      data: {
+        title: 'SmartAdmin Intel'
+      }
+    })
 
-        .state('app.smartAdmin.appLayout', {
-            url: '/app-layout',
-            data: {
-                title: 'App Layout'
-            },
-            views: {
-                "content@app": {
-                    templateUrl: 'app/smart-admin/views/app-layout.html'
-                }
-            }
-        })
+    .state('app.smartAdmin.appLayout', {
+      url: '/app-layout',
+      data: {
+        title: 'App Layout'
+      },
+      views: {
+        "content@app": {
+          templateUrl: 'app/smart-admin/views/app-layout.html'
+        }
+      }
+    })
 
-        .state('app.smartAdmin.diffVer', {
-            url: '/different-versions',
-            data: {
-                title: 'Different Versions'
-            },
-            views: {
-                "content@app": {
-                    templateUrl: 'app/smart-admin/views/different-versions.html'
-                }
-            }
-        })
+    .state('app.smartAdmin.diffVer', {
+      url: '/different-versions',
+      data: {
+        title: 'Different Versions'
+      },
+      views: {
+        "content@app": {
+          templateUrl: 'app/smart-admin/views/different-versions.html'
+        }
+      }
+    })
 
-        .state('app.smartAdmin.appLayouts', {
-            url: '/app-layouts',
-            data: {
-                title: 'App Layouts'
-            },
-            views: {
-                "content@app": {
-                    templateUrl: 'app/smart-admin/views/app-layouts.html'
-                }
-            }
-        })
+    .state('app.smartAdmin.appLayouts', {
+      url: '/app-layouts',
+      data: {
+        title: 'App Layouts'
+      },
+      views: {
+        "content@app": {
+          templateUrl: 'app/smart-admin/views/app-layouts.html'
+        }
+      }
+    })
 
-        .state('app.smartAdmin.prebuiltSkins', {
-            url: '/prebuilt-skins',
-            data: {
-                title: 'Prebuilt Skins'
-            },
-            views: {
-                "content@app": {
-                    templateUrl: 'app/smart-admin/views/prebuilt-skins.html'
-                }
-            }
-        })
+    .state('app.smartAdmin.prebuiltSkins', {
+      url: '/prebuilt-skins',
+      data: {
+        title: 'Prebuilt Skins'
+      },
+      views: {
+        "content@app": {
+          templateUrl: 'app/smart-admin/views/prebuilt-skins.html'
+        }
+      }
+    })
+    .state('app.smartAdmin.invoice', {
+      url: '/invoice-check',
+      data: {
+        title: 'Invoice'
+      },
+      views: {
+        "content@app": {
+          templateUrl: 'app/misc/views/invoice.html'
+        }
+      }
+    })
 });
