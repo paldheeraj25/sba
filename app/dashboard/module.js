8f95ae8d94c1dd98b02ba74499568bc4115bddb8
Dheeraj <dheeraj.pal@rubiconred.com>
2017-08-12 13:04:39 +0530

    code pushe

--- app/dashboard/module.js
+++ app/dashboard/module.js
@@ -52,7 +52,17 @@ angular.module('app.dashboard', [
           "content@app": {
             templateUrl: 'app/dashboard/map-database.tpl.html',
             controller: function ($scope) {
+              $scope.choices = [{ id: 'choice1' }, { id: 'choice2' }];
 
+              $scope.addNewChoice = function () {
+                var newItemNo = $scope.choices.length + 1;
+                $scope.choices.push({ 'id': 'choice' + newItemNo });
+              };
+
+              $scope.removeChoice = function () {
+                var lastItem = $scope.choices.length - 1;
+                $scope.choices.splice(lastItem);
+              };
             },
             resolve: {
               scripts: function (lazyScript) {
