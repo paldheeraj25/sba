6750916c276dfb38f437f82795a94209b5f3a77d
Dheeraj <dheeraj.pal@rubiconred.com>
2017-08-16 23:39:23 +0530

    user

--- app/smart-admin/controllers/UserListController.js
+++ app/smart-admin/controllers/UserListController.js
@@ -6,12 +6,24 @@ angular.module('app.smartAdmin').controller('UserListController', function ($sco
   // Live Stats Tab
   var databaseRef = firebase.database().ref().child('role');
   var databaseUserRef = firebase.database().ref().child('user');
-  databaseUserRef.once('value').then(function (snapshot) {
-    console.log(snapshot.val());
-    if (snapshot.val() !== null) {
-      $scope.user = snapshot.val();
-      return $scope.user;
-    }
-  });
+  function getUsers() {
+    databaseUserRef.orderByChild("active").equalTo(1).once('value').then(function (snapshot) {
+      console.log(snapshot.val());
+      if (snapshot.val() !== null) {
+        $scope.user = snapshot.val();
+        return $scope.user;
+      }
+    });
+  }
+  getUsers();
+
+  $scope.deletUser = function (user) {
+    //console.log(user);
+    databaseUserRef.orderByChild("email").equalTo(user.email).once('value').then(function (snapshot) {
+      databaseUserRef.child(_.keys(snapshot.val())[0]).update({ active: 0 }).then(function () {
+        getUsers();
+      });
+    });
+  }
 
 });
